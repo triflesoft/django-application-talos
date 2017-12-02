de1e950f6c40797c049ca1e455d95cc1c3ffb9a6
adontz <adontz@gmail.com>
2017-12-02 07:02:20 +0400

    Use callable for hostname default value.

--- applications/talos/models.py
+++ applications/talos/models.py
@@ -52,6 +52,10 @@ def _create_class_by_name(class_name):
 _hostname = gethostname()
 
 
+def _default_hostname():
+    return _hostname
+
+
 class AbstractReplicatableModel(models.Model):
     id = models.AutoField(unique=True, primary_key=True)
     uuid = models.UUIDField(unique=True, default=uuid4, editable=False)
@@ -59,8 +63,8 @@ class AbstractReplicatableModel(models.Model):
     modified_at = models.DateTimeField(auto_now=True, editable=False)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, related_name='+', on_delete=models.CASCADE, editable=False)
     modified_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, related_name='+', on_delete=models.CASCADE, editable=False)
-    created_on = models.CharField(max_length=255, default=_hostname)
-    modified_on = models.CharField(max_length=255, default=_hostname)
+    created_on = models.CharField(max_length=255, default=_default_hostname)
+    modified_on = models.CharField(max_length=255, default=_default_hostname)
 
     class Meta:
         abstract = True
